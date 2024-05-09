cee1b2df98a0cea839521aa8ab745a0c3f9b376d
Fan01abx <136188642+Fan01abx@users.noreply.github.com>
2024-05-09 08:51:18 +0530

    Add files via upload

---
+++ steps.cpp
@@ -0,0 +1,50 @@
+ 4 assignment weka tool
+
+1. Open file 
+2. Choose arff file
+3. Click cluster 
+4. Choose farthest first 
+5. Click use training set
+6. Start
+7. Below result list
+8. Right click
+9. Visualize cluster assignment 
+10. Same for simple kmean algo
+11. Repeat steps from 3 onwords
+
+: 5 assignment 
+
+1.Open moviereduced data (sir detil) file
+2. Ok 
+3. Choose textdirectoryloader
+4. Ok
+5. Filter
+6. Unsupervised 
+7. Attribute 
+8. String to vector
+9. Remove 2-10 
+10. Visualize all
+11. Again choose numeric to nominal
+12. Visualize stop
+13. Select classify
+14. Start --stop
+15. Select select attributes 
+16. Start
+17. Visualize
+
+ 6 assignment 
+
+Code
+
+1. Preprocess
+2. Open file .arff
+3. Classify
+4. Choose  decision stump
+5. Start
+6. Again choose smoreg
+7. Start 
+8. Again choose IBK In LAZY
+9. Start 
+10. Visualize
+
+ 
